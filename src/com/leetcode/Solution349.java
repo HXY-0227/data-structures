dfcf575adf0a4edc5125c254ec71ca06d065c41e
HXY-0227 <1473706220@qq.com>
2019-09-05 18:58:46 +0800

    提交人：韩星宇 提交说明：leetcode_349

--- src/com/leetcode/Solution349.java
+++ src/com/leetcode/Solution349.java
@@ -9,7 +9,7 @@ import java.util.Set;
  * 给定两个数组，编写一个函数来计算它们的交集。
  * nums1 = [1,2,2,1], nums2 = [2,2]   ---->  [2]
  */
-public class Solution394 {
+public class Solution349 {
 
     public static int[] intersection(int[] nums1, int[] nums2) {
 
