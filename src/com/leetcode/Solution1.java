c53a775afb47025fc753ae8d266138986f91249a
HXY-0227 <1473706220@qq.com>
2019-12-22 23:32:32 +0800

    提交人：韩星宇 提交说明：leetcode_1

--- src/com/leetcode/Solution1.java
+++ src/com/leetcode/Solution1.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 给定 nums = [2, 7, 11, 15], target = 9  返回 [0, 1]
  */
-public class Soultion1 {
+public class Solution1 {
 
     /**
      * 暴力破解
