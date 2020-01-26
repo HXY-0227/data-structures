6e82e476965b995ef0ead7d3c4e359c3e4511e95
HXY-0227 <1473706220@qq.com>
2020-01-26 22:40:14 +0800

    提交人：韩星宇 提交说明：leetcode347

---
+++ src/com/leetcode/Solution347.java
@@ -0,0 +1,48 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * leetcode347 https://leetcode-cn.com/problems/top-k-frequent-elements/
+ */
+public class Solution347 {
+
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 01 - 02;
+            }
+        });
+
+        for (int key: map.keySet()) {
+            if (queue.size() < k) {
+                queue.add(key);
+            } else if (map.get(key) > map.get(queue.peek())) {
+                queue.remove();
+                queue.add(key);
+            }
+        }
+
+        LinkedList<Integer> result = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            result.add(queue.remove());
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,1,1,2,2,3,3,3};
+//        topKFrequent(arr,2);
+        System.out.println(topKFrequent(arr, 2));
+    }
+}
