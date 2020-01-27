2155bd88d175b8d3168e1c16ebe621f9ceb36322
HXY-0227 <1473706220@qq.com>
2020-01-27 22:17:13 +0800

    提交人：韩星宇 提交说明：链表实现队列

---
+++ src/com/queue/LinkedListQueue.java
@@ -0,0 +1,90 @@
+package com.queue;
+
+/**
+ * 用链表实现队列，只在对首出队，在队尾入队，这样保证出队和入队都能保证O(1)的时间复杂度
+ *
+ * @author HXY
+ * @since 2020-1-27 抗击病毒第三天
+ */
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private class Node{
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this(e, null);
+        }
+
+        public Node(){
+            this(null, null);
+        }
+
+        @Override
+        public String toString(){
+            return e.toString();
+        }
+    }
+
+    private Node head, tail;
+    private int size;
+
+    public LinkedListQueue() {
+        this.tail = null;
+        this.head = null;
+        size = 0;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if (tail == null) {
+            tail = new Node(e);
+            head = tail;
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("queue is empty");
+        }
+
+        Node node = head;
+        head = head.next;
+        node.next = null;
+
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return node.e;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("queue is empty");
+        }
+
+        return head.e;
+    }
+}
