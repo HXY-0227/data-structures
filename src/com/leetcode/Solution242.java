feb226e1034344bd13c418c6abdf4b964de865cf
HXY-0227 <1473706220@qq.com>
2020-02-29 16:05:42 +0800

    提交人：韩星宇 提交说明：leetCode242

---
+++ src/com/leetcode/Solution242.java
@@ -0,0 +1,45 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * @Author HXY
+ * @Date 2020/2/29
+ */
+public class Solution242 {
+    // 方案1
+/*    public static boolean isAnagram(String s, String t) {
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        Arrays.sort(chars);
+        Arrays.sort(chart);
+        return Arrays.equals(chars, chart);
+    }*/
+
+    // 方案2
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+}
